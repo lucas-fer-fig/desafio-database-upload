e3743ee704f6f09f6f84e8be6e94c08f3f81d834
lucas-fer-fig <figueiredo.lucasfernandes@gmail.com>
2020-10-26 13:35:04 -0400

    Novo upload

--- src/__tests__/Transaction.spec.ts
+++ src/__tests__/Transaction.spec.ts
@@ -197,7 +197,7 @@ describe('Transaction', () => {
   });
 
   it('should be able to import transactions', async () => {
-    /* const transactionsRepository = getRepository(Transaction);
+    const transactionsRepository = getRepository(Transaction);
     const categoriesRepository = getRepository(Category);
 
     const importCSV = path.resolve(__dirname, 'import_template.csv');
@@ -235,6 +235,6 @@ describe('Transaction', () => {
           type: 'outcome',
         }),
       ]),
-    ); */
+    );
   });
 });
